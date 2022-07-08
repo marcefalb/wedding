92ea9dacef3e16dd8b16300a662b94ddff3c9586
marcefalb <73983720+marcefalb@users.noreply.github.com>
2022-07-08 22:30:44 +0300

    first commit

---
+++ assets/js/index.js
@@ -0,0 +1,65 @@
+anime({
+  targets: '.pt__top-left',
+  translateX: '40vw',
+  duration: 800,
+  easing: 'spring(1, 80, 10, 0)'
+});
+anime({
+  targets: '.pt__top-right',
+  translateX: '-18vw',
+  duration: 800,
+  easing: 'spring(1, 80, 10, 0)'
+});
+anime({
+  targets: '.pt__bottom-left',
+  translateX: '18vw',
+  duration: 800,
+  easing: 'spring(1, 80, 10, 0)'
+});
+anime({
+  targets: '.pt__bottom-right',
+  translateX: '-40vw',
+  duration: 800,
+  easing: 'spring(1, 80, 10, 0)'
+});
+anime({
+  targets: '.invite__wrapper',
+  opacity: 1,
+  duration: 1650,
+  easing: 'easeInOutQuint',
+});
+anime({
+  targets: '.invite__top',
+  opacity: 1,
+  duration: 1000,
+  easing: 'easeInOutQuint',
+  delay: 400,
+});
+anime({
+  targets: '.invite__names',
+  opacity: 1,
+  duration: 1000,
+  easing: 'easeInOutQuint',
+  delay: 900,
+});
+anime({
+  targets: '.invite__slogan',
+  opacity: 1,
+  duration: 1000,
+  easing: 'easeInOutQuint',
+  delay: 1300,
+});
+anime({
+  targets: '.invite__date',
+  opacity: 1,
+  duration: 1000,
+  easing: 'easeInOutQuint',
+  delay: 1700,
+});
+anime({
+  targets: '.invite__link a',
+  opacity: 1,
+  duration: 1000,
+  easing: 'easeInOutQuint',
+  delay: 2100,
+});
